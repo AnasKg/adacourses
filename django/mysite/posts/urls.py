41b211343d7b5730d12969afd702ef1474f1e6d1
Anas <anas.ergeshov.98@gmail.com>
2023-05-13 19:34:16 +0600

    users list and user detail

--- django/mysite/posts/urls.py
+++ django/mysite/posts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.post_list, name='post_list'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail')
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('users/', views.users_list, name='users_list'),
+    path('users/<int:user_id>/', views.user_detail, name='user_detail')
 ]
--- django/mysite/posts/views.py
+++ django/mysite/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import Http404
 from .models import Post, Comment
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -19,3 +21,15 @@ def post_detail(request, post_id):
     context = {'post': post, 'comments': comments}
 
     return render(request, 'posts/post_detail.html', context=context)
+
+
+def users_list(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'posts/user_list.html', context=context)
+
+
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    context = {'user': user}
+    return render(request, 'posts/user_detail.html', context=context)
