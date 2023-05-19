8be8269d8209c0e6c055b0529c69def5f8a576fd
Anas <anas.ergeshov.98@gmail.com>
2023-05-19 21:01:16 +0600

    tasks

--- django/mysite/posts/urls.py
+++ django/mysite/posts/urls.py
@@ -9,18 +9,19 @@ urlpatterns = [
 
     # path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('posts/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:post_id>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
     path('users/', views.users_list, name='users_list'),
     path('users/<int:user_id>/', views.user_detail, name='user_detail'),
 
-    path('posts/create/', views.create_post, name = 'create_post'),
+    path('posts/create/', views.PostCreateView.as_view(), name = 'create_post'),
     # path('posts/create/', views.PostView.as_view(), name = 'create_post'),
     
-    path('posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('posts/<int:post_id>/edit/', views.PostUpdateView.as_view(), name='edit_post'),
 
     path('products/', views.see_product, name='product_list'),
     path('products/create/', views.create_product, name='create_product'),
 
-    path('posts/<int:post_id>/comments/<int:comment_id>/', views.edit_comment, name='edit_comment')
-
+    path('posts/<int:post_id>/comments/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('post/<int:post_id>/comment/', views.CommentCreateView.as_view(), name='create_comment'),
 ]
--- django/mysite/posts/views.py
+++ django/mysite/posts/views.py
@@ -1,9 +1,11 @@
+from typing import Any, Dict
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from django.http import Http404
 from .models import Post, Comment, Product
 from django.contrib.auth.models import User
 from .forms import MyForm, PostForm, ProductForm, CommentForm
 from django.views import View, generic
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -18,7 +20,7 @@ def post_list(request):
 
 class PostListView(generic.ListView):
     # model = Post
-    queryset = Post.objects.all().order_by('title')
+    queryset = Post.objects.all().order_by('-created_at')
     context_object_name = 'posts'
     template_name = 'posts/post_list.html'
 
@@ -28,22 +30,54 @@ class PostDetailView(generic.DetailView):
     context_object_name = 'post'
     template_name = 'posts/post_detail.html'
     pk_url_kwarg = 'post_id'
-    
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        context['comments'] = Comment.objects.filter(post=post)
+        context['form'] = CommentForm()
+        return context
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = ('title', 'content', 'author')
+    template_name = 'posts/create_post.html'
+    # success_url = '/posts/'
+
+    def get_success_url(self):
+        post = self.object
+        return reverse_lazy('post_detail', kwargs={'post_id': post.id})
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = ('title', 'content', 'author')
+    template_name = 'posts/edit_post.html'
+    # success_url = '/posts/'
+    pk_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        post = self.object
+        return reverse_lazy('post_detail', kwargs={'post_id': post.id})
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    fields = ['author', 'content']
+    template_name = 'posts/comment_form.html'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'post_id': self.kwargs['post_id']})
 
 
-
-# class PostView(View):
-
-#     def get(self, request):
-#         posts = Post.objects.all()
-#         context = {'posts': posts}
-#         return render(request, 'posts/post_list.html', context=context)
-    
-#     def post(self, request):
-#         form = PostForm(request.POST)
-#         if form.is_valid():
-#             post = form.save()
-#             return redirect('post_detail', post_id = post.id)
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+    pk_url_kwarg = 'post_id'
 
 
 def post_detail(request, post_id):
